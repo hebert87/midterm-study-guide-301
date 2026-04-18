53d17b9fa6355ce26580c66ebd5a60e6e55f0287
Jessica H <jessicahebert75@gmail.com>
2026-04-18 13:23:08 -0700

    Implement unit tests for midterm

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -1,4 +1,8 @@
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 import org.junit.jupiter.api.Test;
 
@@ -17,9 +21,67 @@ public class PracticeTest {
         // Largest: 9, Smallest: -5, Difference: 9 - -5 = 14
         assertEquals(14, actual);
     }
-    
 
-    // TODO: Make tests for each problem you solve
+    @Test
+    void testMaxDiffSingleValue() {
+        int[] nums = {5};
+        assertEquals(0, Practice.maxDiff(nums));
+    }
+
+
+    // longestWord
+    @Test
+    void testLongestWord() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("a", "apple");
+        map.put("b", "apricot");
+        map.put("c", "banana");
+
+        assertEquals("apricot", Practice.longestWord(map, 'a'));
+    }
+
+    @Test
+    void testLongestWordNoneMatch() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("a", "banana");
+
+        assertEquals("", Practice.longestWord(map, 'z'));
+    }
+
+    //countWords
+    @Test
+    void testCountWords() {
+        ArrayList<String> words = new ArrayList<>();
+        words.add("hi");
+        words.add("hello");
+        words.add("world");
+        words.add("a");
+
+        assertEquals(2, Practice.countWords(words, 2, 6));
+    }
+
+    //differenceInOddEven
+    @Test
+    void testOddEvenDifference() {
+        HashSet<Integer> nums = new HashSet<>();
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+        nums.add(4);
+
+        assertEquals(0, Practice.differenceInOddEven(nums));
+    }
+    
+    //secondLargestNum
+    @Test
+    void testSecondLargest() {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(10, 1);
+        map.put(5, 2);
+        map.put(20, 3);
+
+        assertEquals(10, Practice.secondLargestNum(map));
+    }
     
 }
 
